7f1b575e76e9a7135f2f4bf21bd6155b0cf1d58b
Lucas Bastos Franco <139567823+LucasfcBastos@users.noreply.github.com>
2025-03-16 17:31:16 -0300

    terminando os codigo da lista de exercícios

--- 5 período/AG/Ciclo 01/Lista de Preparação/part2.c
+++ 5 período/AG/Ciclo 01/Lista de Preparação/part2.c
@@ -2,49 +2,62 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <string.h>
+
 #ifdef _WIN32
     #include <windows.h>
 #endif
 
+#define MAX_PALAVRAS 10000
+#define TAMANHO_MAX 50
+#define MAX_PLAYERS 100
+
+typedef struct {
+    char nome[50];
+    int pontos;
+} Jogador;
+
+// Funções auxiliares para tempo e tela
 void clockTime(int x) {
     #ifdef _WIN32
-        Sleep(x); // Windows
+        Sleep(x);
     #else
-        usleep(x * 1000); // Linux
+        usleep(x * 1000);
     #endif
 }
 
 void clearScreen() {
     #ifdef _WIN32
-        system("cls"); // Windows
+        system("cls");
     #else
-        system("clear"); // Linux
+        system("clear");
     #endif
 }
 
-void trocou(int *a, int *b) {
+void trocar(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-void imprimir(int list[], int MAX){
-    for(int i = 0; i < MAX - 1; i++){
+void imprimir(int list[], int MAX) {
+    for (int i = 0; i < MAX; i++) {
         printf("| %d ", list[i]);
     }
     printf("|\n\n");
 }
 
+// Implementação do Insertion Sort
 int Insertion(int MAX, int type) {
     int list[MAX], cha;
 
-    for(int i = 0; i < MAX; i++){
+    for (int i = 0; i < MAX; i++) {
         list[i] = (rand() % 99) + 1;
     }
 
     imprimir(list, MAX);
 
-    for(int i = 0; i < MAX-1; i++){
+    for (int i = 1; i < MAX; i++) {
         cha = list[i];
         int j = i - 1;
 
@@ -55,34 +68,26 @@ int Insertion(int MAX, int type) {
         list[j + 1] = cha;
 
         imprimir(list, MAX);
-
     }
 
     return 0;
 }
 
+// Implementação do QuickSort com escolha tradicional do pivô
 int apend(int list[], int min, int max, int type, int choice, int *perc) {
-    int pivo;
-    if (choice == 1) {
-        pivo = list[max];
-    } else if (choice == 2) {
-        pivo = list[min];
-    }
+    int pivo = (choice == 1) ? list[max] : list[min];
     int i = min - 1;
     (*perc)++;
 
     for (int j = min; j < max; j++) {
-        if (list[j] <= pivo && type == 1) {
+        if ((list[j] <= pivo && type == 1) || (list[j] >= pivo && type == 2)) {
             i++;
-            trocou(&list[i], &list[j]);
-        } else if (list[j] >= pivo && type == 2) {
-            i++;
-            trocou(&list[i], &list[j]);
+            trocar(&list[i], &list[j]);
         }
     }
-    trocou(&list[i + 1], &list[max]);
-    printf("percorrida %d = ", *perc);
-    imprimir(list, max+1);
+    trocar(&list[i + 1], &list[max]);
+    printf("Percorrida %d = ", *perc);
+    imprimir(list, max + 1);
     return i + 1;
 }
 
@@ -95,75 +100,154 @@ void Quick(int list[], int min, int max, int type, int choice, int *perc) {
 }
 
 int PreQuick(int MAX, int type, int choice) {
-    int list[MAX];
-    int perc = 0;
+    int list[MAX], perc = 0;
 
-    for(int i = 0; i < MAX; i++){
+    for (int i = 0; i < MAX; i++) {
         list[i] = (rand() % 99) + 1;
     }
 
     imprimir(list, MAX);
-
     Quick(list, 0, MAX - 1, type, choice, &perc);
-
     imprimir(list, MAX);
 
     return 0;
 }
 
+// QuickSort com Mediana de Três
+int medianaDeTres(int list[], int esq, int dir) {
+    int meio = (esq + dir) / 2;
+
+    if (list[esq] > list[meio]) trocar(&list[esq], &list[meio]);
+    if (list[esq] > list[dir]) trocar(&list[esq], &list[dir]);
+    if (list[meio] > list[dir]) trocar(&list[meio], &list[dir]);
+
+    trocar(&list[meio], &list[dir]);
+    return list[dir];
+}
+
+void QuickSortMediana(int list[], int esq, int dir) {
+    if (esq < dir) {
+        int pi = medianaDeTres(list, esq, dir);
+        QuickSortMediana(list, esq, pi - 1);
+        QuickSortMediana(list, pi + 1, dir);
+    }
+}
+
+// Inserção de jogador no ranking ordenado (InsertionSort)
+void inserirJogador(Jogador ranking[], int *tamanho, Jogador novoJogador) {
+    int i = *tamanho - 1;
+
+    while (i >= 0 && ranking[i].pontos < novoJogador.pontos) {
+        ranking[i + 1] = ranking[i];
+        i--;
+    }
+
+    ranking[i + 1] = novoJogador;
+    (*tamanho)++;
+}
+
+void imprimirRanking(Jogador ranking[], int tamanho) {
+    printf("\n--- Ranking de Jogadores ---\n");
+    for (int i = 0; i < tamanho; i++) {
+        printf("%d. %s - %d pontos\n", i + 1, ranking[i].nome, ranking[i].pontos);
+    }
+    printf("----------------------------\n");
+}
+
+// QuickSort para ordenação de palavras
+void QuickSortPalavras(char lista[][TAMANHO_MAX], int esq, int dir) {
+    if (esq < dir) {
+        int pi = esq;
+        for (int j = esq + 1; j <= dir; j++) {
+            if (strcmp(lista[j], lista[dir]) < 0) {
+                pi++;
+                char temp[TAMANHO_MAX];
+                strcpy(temp, lista[pi]);
+                strcpy(lista[pi], lista[j]);
+                strcpy(lista[j], temp);
+            }
+        }
+        char temp[TAMANHO_MAX];
+        strcpy(temp, lista[pi]);
+        strcpy(lista[pi], lista[dir]);
+        strcpy(lista[dir], temp);
+
+        QuickSortPalavras(lista, esq, pi - 1);
+        QuickSortPalavras(lista, pi + 1, dir);
+    }
+}
+
 void menu() {
     printf("1 - Metodo Insertion\n");
     printf("2 - Metodo Quick\n");
+    printf("3 - QuickSort com Mediana de Tres\n");
+    printf("4 - Inserir Jogador no Ranking\n");
+    printf("5 - Ordenar Palavras com QuickSort\n");
     printf("0 - Sair\n\n");
 }
 
 int main() {
     int choice, num, type, QuickChoice;
+    Jogador ranking[MAX_PLAYERS];
+    int numJogadores = 0;
+
+    char palavras[MAX_PALAVRAS][TAMANHO_MAX];
+    int numPalavras = 0;
+
     clearScreen();
 
     do {
         menu();
-        printf("Escolha\n");
+        printf("Escolha: ");
         scanf("%d", &choice);
 
         switch (choice) {
-        case 1:
-            printf("\n=-=-=-=-= INSERTION =-=-=-=-=\n\n");
-            printf("Escolha quanto elementos tem na lista: \n");
-            scanf("%d", &num);
-            printf("1 - Cresente ou 2 - Decresente: \n");
-            scanf("%d", &type);
-            printf("\n");
-            Insertion(num, type);
-            clockTime(10000);
-            clearScreen();
-            break;
-        case 2:
-            printf("\n=-=-=-=-= QUICK =-=-=-=-=\n\n");
-            printf("Escolha quanto elementos tem na lista: \n");
-            scanf("%d", &num);
-            printf("1 - Cresente ou 2 - Decresente: \n");
-            scanf("%d", &type);
-            printf("1 - Pivo no final ou 2 - Pivo no comeco: \n");
-            scanf("%d", &QuickChoice);
-            printf("\n");
-            PreQuick(num, type, QuickChoice);
-            clockTime(10000);
-            clearScreen();
-            break;
-        case 0:
-            printf("\nSAINDO\n");
-            clockTime(2500);
-            clearScreen();
-            break;
-        default:
-            clearScreen();
-            printf("\n\tOPCAO INVALIDA\n");
-            clockTime(2500);
-            clearScreen();
-            break;
-        }
+            case 1:
+                printf("Tamanho da lista: ");
+                scanf("%d", &num);
+                printf("1 - Crescente ou 2 - Decrescente: ");
+                scanf("%d", &type);
+                Insertion(num, type);
+                break;
 
+            case 2:
+                printf("Tamanho da lista: ");
+                scanf("%d", &num);
+                printf("1 - Crescente ou 2 - Decrescente: ");
+                scanf("%d", &type);
+                printf("1 - Pivo no final ou 2 - Pivo no começo: ");
+                scanf("%d", &QuickChoice);
+                PreQuick(num, type, QuickChoice);
+                break;
+
+            case 3:
+                printf("Tamanho da lista: ");
+                scanf("%d", &num);
+                int list[num];
+                for (int i = 0; i < num; i++) list[i] = rand() % 99 + 1;
+                imprimir(list, num);
+                QuickSortMediana(list, 0, num - 1);
+                imprimir(list, num);
+                break;
+
+            case 4:
+                printf("Nome do jogador: ");
+                scanf("%s", ranking[numJogadores].nome);
+                printf("Pontos: ");
+                scanf("%d", &ranking[numJogadores].pontos);
+                inserirJogador(ranking, &numJogadores, ranking[numJogadores]);
+                imprimirRanking(ranking, numJogadores);
+                break;
+
+            case 5:
+                printf("Ordenando palavras...\n");
+                QuickSortPalavras(palavras, 0, numPalavras - 1);
+                break;
+
+            case 0:
+                printf("Saindo...\n");
+                break;
+        }
     } while (choice != 0);
 
     return 0;
